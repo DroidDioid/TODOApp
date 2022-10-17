b0f22ae52e67469e9cba2e5baed02ba9b2096d36
Timofey <tim20045@mail.ru>
2022-10-17 15:26:24 +0700

    Initial commit

---
+++ app/src/main/java/ru/tim/todoapp/TasksActivity.kt
@@ -0,0 +1,11 @@
+package ru.tim.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TasksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasks)
+    }
+}
